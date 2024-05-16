181cd7d4dac5082f4eaf25cb48d83eef5b45c748
marcossFarias <marcossouzacg2004@proton.me>
2024-05-16 08:32:31 -0400

    fix navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,6 +5,12 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
+import logo from '../assets/img/logo.svg'
+import navicon1 from '../assets/img/nav-icon1.svg'
+import navicon2 from '../assets/img/nav-icon2.svg'
+import navicon3 from '../assets/img/nav-icon3.svg'
+
+
 function PortifolioNavBar() {
     cosnt [activeLink, setActiveLink] = userState('home');
     const [scrolled, setScrolled] = userState(false);
@@ -16,40 +22,35 @@ function PortifolioNavBar() {
             } else {
                 setScrolled(false);
             }
-        }
+        };
 
         window.addEventListener("scroll", onScroll);
 
         return () => window.removeEventListener("scroll", onScroll);
-    }, [])
+    }, []);
 
     const onUpdateActiveLink = (value) => {
         setActiveLink(value)
-    }
+    };
 
     return (
         <Navbar expand="lg" className={scrolled ? 'scrolled' : ''}>
         <Container>
-            <Navbar.Brand href="#home">My Personal Portifolio</Navbar.Brand>
+            <Navbar.Brand href="#home"> <img scr={logo} alt='logo'/>My Personal Portifolio</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
                 <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
                 <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
                 <Nav.Link href="#about-me" className={activeLink === 'about-me' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('about-me')}>About me</Nav.Link>
-
-                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                    Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                    Separated link
-                </NavDropdown.Item>
-                </NavDropdown>
             </Nav>
+            <span className='navbar-text'>
+                <div className='social-icon'>
+                    <a href='#'><img src={navicon1} alt='' /></a>
+                    <a href='#'><img src={navicon2} alt='' /></a>
+                    <a href='#'><img src={navicon3} alt='' /></a>
+                </div>
+            </span>
             </Navbar.Collapse>
         </Container>
         </Navbar>
