e0f63aa56b69ceb8c98d10dfa6a077f61168e320
marcossFarias <marcossouzacg2004@proton.me>
2024-05-16 20:19:54 -0400

    add about me page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import NavBar from './components/NavBar';
+import AboutMe from './components/AboutMe';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<NavBar />} />
+          <Route path="/about-me" component={AboutMe} />
         </Routes>
       </BrowserRouter>
     </div>
